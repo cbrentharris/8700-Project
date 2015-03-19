b1f746e4257379b5b306d598eabe3ec516d0d836
Christopher Harris <chris@inflection>
2015-03-18 19:57:22 -0500

    updating urls and settings

--- CarFinder/settings.py
+++ CarFinder/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'detail',
+    'search'
 )
 
 MIDDLEWARE_CLASSES = (
--- CarFinder/urls.py
+++ CarFinder/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'CarFinder.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomePageView.as_view(), name='home')
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'carfinder/login.html'}, name='login'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page' : 'login'}, name='logout'),
 )

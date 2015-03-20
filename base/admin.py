4b89dc6dd9c23e7fbbc83e2a8288915161a3c03e
Christopher Harris <chris@inflection>
2015-03-19 22:24:26 -0500

    registering models

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from models import *
 
-admin.register(Vendor)
-admin.register(Car)
-admin.register(Trim)
-admin.register(Option)
-admin.register(Make)
+admin.site.register(Vendor)
+admin.site.register(Car)
+admin.site.register(Trim)
+admin.site.register(Option)
+admin.site.register(Make)

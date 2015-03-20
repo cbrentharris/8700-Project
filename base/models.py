e66b6230f9724141c7415d70aa932cecb32ce646
Christopher Harris <chris@inflection>
2015-03-19 22:13:13 -0500

    reordering to avoid conflicts

--- base/models.py
+++ base/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class ListingImage(models.Model):
-    image = models.ImageField(upload_to='listings/images')
-    listing = models.ForeignKey(Listing)
+class Car(models.Model):
+    doors = models.IntegerField()
+    make = models.ForeignKey(Make)
+    model = models.CharField(max_length=200)
+    year = models.IntegerField()
 
 class Listing(models.Model):
     vendor = models.ForeignKey(Vendor)
     car = models.ForeignKey(Car)
     date_created = models.DateTimeField(auto_now_add=True)
+
+class ListingImage(models.Model):
+    image = models.ImageField(upload_to='listings/images')
+    listing = models.ForeignKey(Listing)
 
 class Vendor(models.Model):
     contact = models.ForeignKey(User,unique=True)
@@ -27,12 +33,6 @@ class Trim(models.Model):
 
     def __unicode__(self):
         return self.name
-
-class Car(models.Model):
-    doors = models.IntegerField()
-    make = models.ForeignKey(Make)
-    model = models.CharField(max_length=200)
-    year = models.IntegerField()
 
 class Option(models.Model):
     name = models.CharField(max_length=200)
